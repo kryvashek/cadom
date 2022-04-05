ac30aa879ec28eca07aa6ede47e7c4376e76109b
Artyom Sakharilenko <a.sakharilenko@tinkoff.ru>
2022-04-06 00:35:30 +0300

    CodePlaceChain implementation.

--- src/place/mod.rs
+++ src/place/mod.rs
@@ -1,4 +1,8 @@
-use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use cubob::display_list_from_iter;
+use std::{
+    fmt::{Debug, Display, Formatter, Result as FmtResult},
+    iter::FusedIterator,
+};
 
 #[derive(Clone, PartialEq, Eq)]
 pub struct CodePlace {
@@ -30,6 +34,70 @@ macro_rules! place {
     () => {
         $crate::CodePlace::new(file!(), line!(), column!())
     };
+}
+
+#[derive(Clone, PartialEq, Eq)]
+pub struct CodePlaceChain {
+    head: CodePlace,
+    tail: Option<Box<CodePlaceChain>>,
+}
+
+impl CodePlaceChain {
+    pub fn prepend(self, place: CodePlace) -> Self {
+        Self {
+            head: place,
+            tail: Some(Box::new(self)),
+        }
+    }
+}
+
+impl From<CodePlace> for CodePlaceChain {
+    fn from(src: CodePlace) -> Self {
+        Self {
+            head: src,
+            tail: None,
+        }
+    }
+}
+
+impl<'a> IntoIterator for &'a CodePlaceChain {
+    type Item = &'a CodePlace;
+    type IntoIter = CodePlaceChainIter<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        CodePlaceChainIter(Some(self))
+    }
+}
+
+pub struct CodePlaceChainIter<'a>(Option<&'a CodePlaceChain>);
+
+impl<'a> Iterator for CodePlaceChainIter<'a> {
+    type Item = &'a CodePlace;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.0 {
+            None => None,
+            Some(entry) => {
+                let current = &entry.head;
+                self.0 = entry.tail.as_deref();
+                Some(current)
+            }
+        }
+    }
+}
+
+impl FusedIterator for CodePlaceChainIter<'_> {}
+
+impl Display for CodePlaceChain {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        display_list_from_iter(f, self.into_iter())
+    }
+}
+
+impl Debug for CodePlaceChain {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        Display::fmt(self, f)
+    }
 }
 
 #[cfg(test)]
--- src/place/tests.rs
+++ src/place/tests.rs
@@ -35,3 +35,31 @@ fn place_debug_pretty() {
         "src/place/tests.rs:4:37"
     );
 }
+
+#[test]
+fn chain_display() {
+    let chain = CodePlaceChain::from(TEST_PLACE.clone()).prepend(place!());
+    assert_eq!(
+        &format!("{}", chain),
+        "[src/place/tests.rs:41:66, src/place/tests.rs:3:71]"
+    );
+    let chain = CodePlaceChain::from(TEST_PLACE_MACRO.clone()).prepend(place!());
+    assert_eq!(
+        &format!("{}", chain),
+        "[src/place/tests.rs:46:72, src/place/tests.rs:4:37]"
+    );
+}
+
+#[test]
+fn chain_debug() {
+    let chain = CodePlaceChain::from(TEST_PLACE.clone()).prepend(place!());
+    assert_eq!(
+        &format!("{:?}", chain),
+        "[src/place/tests.rs:55:66, src/place/tests.rs:3:71]"
+    );
+    let chain = CodePlaceChain::from(TEST_PLACE_MACRO.clone()).prepend(place!());
+    assert_eq!(
+        &format!("{:?}", chain),
+        "[src/place/tests.rs:60:72, src/place/tests.rs:4:37]"
+    );
+}
